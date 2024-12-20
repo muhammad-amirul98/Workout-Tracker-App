2ef17b278b147b257989b383f36e83de3020d28c
muhammad-amirul98 <moamirul1998@gmail.com>
2024-12-20 21:03:05 +0800

    Moving Log Out to Nav Bar

--- tsapp/src/App.tsx
+++ tsapp/src/App.tsx
@@ -7,16 +7,35 @@ import Typography from "@mui/material/Typography";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import Exerciselist from "./components/Exerciselist";
 import Login from "./components/Login";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
 
 const queryClient = new QueryClient();
 
-function App() {
+type ExerciselistProps = {
+  logOut?: () => void;
+};
+
+function App({ logOut }: ExerciselistProps) {
   return (
     <Container maxWidth="xl">
       <CssBaseline />
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">Workout Tracker</Typography>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6">Workout Tracker</Typography>
+          </Box>
+          <Button
+            onClick={logOut}
+            color="inherit"
+            sx={{
+              fontSize: "1.25rem", // Matches the font size of Typography variant="h6"
+              fontWeight: "400", // Matches the default Typography font weight
+              textTransform: "none", // Keeps the text case consistent with Typography
+            }}
+          >
+            Log Out
+          </Button>
         </Toolbar>
       </AppBar>
       <QueryClientProvider client={queryClient}>
