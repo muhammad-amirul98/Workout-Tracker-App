9d0d196b954dfa328beea305c9238df38b104064
muhammad-amirul98 <moamirul1998@gmail.com>
2024-12-25 14:13:39 +0800

    Completed Sign Up Front End Page

--- tsapp/src/components/SignUp.tsx
+++ tsapp/src/components/SignUp.tsx
@@ -3,27 +3,42 @@ import { useState } from "react";
 import DialogContent from "@mui/material/DialogContent";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import { Typography } from "@mui/material";
 
 type User = {
   username: string;
   password: string;
   role: "USER";
   email: string;
-  number: string;
 };
 
 function SignUp() {
   //   const [open, setOpen] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
   const [userData, setUserData] = useState<User>({
     username: "",
     password: "",
     role: "USER",
     email: "",
-    number: "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const validateConfirmPassword = () => {
+    if (confirmPassword && confirmPassword !== userData.password) {
+      setError("Passwords do not match!");
+    } else {
+      setError(""); // Clear error if passwords match
+    }
   };
 
   //   const handleSignUp = () => {
@@ -36,7 +51,7 @@ function SignUp() {
   return (
     <>
       <DialogContent>
-        <Stack spacing={2} mt={1}>
+        <Stack spacing={2} alignItems="center" mt={1}>
           <TextField
             label="Name"
             name="username"
@@ -46,7 +61,27 @@ function SignUp() {
           <TextField
             label="Password"
             name="password"
-            value={userData.username}
+            type="password"
+            value={userData.password}
+            onChange={handleChange}
+          ></TextField>
+          <TextField
+            label="Confirm Password"
+            name="password"
+            type="password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            onBlur={validateConfirmPassword}
+          ></TextField>
+          {error && ( // Render error message if it exists
+            <Typography variant="body2" color="error">
+              {error}
+            </Typography>
+          )}
+          <TextField
+            label="Email"
+            name="email"
+            value={userData.email}
             onChange={handleChange}
           ></TextField>
         </Stack>
